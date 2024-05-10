5f15fe3402ac03edff45aa22340138ef38f8bdbc
Al Azad <alazad214@gmail.com>
2024-05-10 11:29:09 +0600

    user profile constactor added

--- lib/models/users_profile.dart
+++ lib/models/users_profile.dart
@@ -1,1 +1,13 @@
-class UserProfile {}
+class UserProfile {
+  final String name;
+  final String email;
+  final String address;
+  final String mobile;
+
+  UserProfile({
+    required this.name,
+    required this.email,
+    required this.address,
+    required this.mobile,
+  });
+}
